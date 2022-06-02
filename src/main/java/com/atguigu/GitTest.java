dfce8dd54f0ba27e2d21a82daa11587319d16f77
RABBIT <rabbitsusu@163.com>
2022-06-02 10:10:51 +0800

    新建

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * Date:2022/6/2
+ * Author:SuSu
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("新建文件");
+    }
+}
